49b83106463850e4c9ff44257b470867c1ebefc1
Samothraki-hbs <hector.belgoderesoria@gmail.com>
2025-12-13 12:34:45 +0100

    changements de la page principale en une flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,37 +1,13 @@
 import revelmasculine from "@assets/data/revelmasculine";
 import PossibleChoice from "@components/PossibleChoice";
-import { StyleSheet, View } from "react-native";
-import Colors from "../../constants/Colors";
+import { FlatList } from "react-native";
 
 export default function MenuScreen() {
   return (
-    <View>
-      <PossibleChoice choice={revelmasculine[0]} />
-      <PossibleChoice choice={revelmasculine[1]} />
-    </View>
+    <FlatList
+      data={revelmasculine}
+      renderItem={({ item }) => <PossibleChoice choice={item} />}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 10,
-    overflow: "hidden",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-    alignSelf: "center",
-  },
-  title: {
-    fontWeight: "600",
-    fontSize: 18,
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-    marginTop: "auto",
-  },
-});
